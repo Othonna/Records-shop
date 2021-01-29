893e8a6ce6fef82f0eafd1fe3ebdb34c7d37f9e6
Janick Othonna <janick.deneux@gmail.com>
2021-01-29 21:35:19 +0100

    Register

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -17,13 +17,4 @@ class IndexController extends AbstractController
          return $this->render('index/index.html.twig');
      }
 
-    /**
-     * @Route("/contact", name="contact")
-     */
-    public function contact(): Response
-    {
-        return $this->render('index/contact.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
-    }
 }
---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="register")
+     */
+    public function register(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            // dd($user);
+            $doctrine = $this->getDoctrine()->getManager();
+            $doctrine->Persist($user);
+            $doctrine->flush();
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
